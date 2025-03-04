4e53ef6705b04b25d4dc23004df6f311b8b55f5f
haseeb-byte <javed.haseeb20001@gmail.com>
2025-03-04 12:48:14 +0100

    Add files via upload

---
+++ esercizio1.py
@@ -0,0 +1,13 @@
+citta=['Milano','Napoli','Roma']
+tmax=['12','23','11']
+tmin=['10','4','21']
+
+for(i>lentgh.tmin):
+    media=tmin/3
+
+for(i>lentgh.tmax):
+    mediatmax=tmax/3
+
+if(citta<media):
+    print(f"le/la citta con temperatura piu bassa della temperarua media sono{citta}")
+
